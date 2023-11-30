3eea5ef535b8bf1ec0831cb9759f0770573b613f
Abhinav Anand <Abhinav011501@gmail.com>
2023-11-30 21:05:37 +0530

    minor: cover bg blur %

--- src/components/Cover.tsx
+++ src/components/Cover.tsx
@@ -11,8 +11,8 @@ interface CoverProps {}
 
 const Cover: FC<CoverProps> = ({}) => {
     return (
-        <div className="flex rounded-lg w-full bg-[url('/images/cover.png')] bg-no-repeat bg-cover bg-center coverReveal">
-            <div className="flex justify-center md:rounded-ld overflow-hidden md:px-10 py-7 bg-blur backdrop-filter backdrop-blur-md w-full gap-2 md:gap-10">
+        <div className="flex rounded-lg w-full bg-[url('/images/cover.png')] bg-no-repeat bg-cover bg-center">
+            <div className="flex justify-center md:rounded-ld overflow-hidden md:px-10 py-7 bg-blur backdrop-filter backdrop-blur-sm w-full gap-2 md:gap-10">
                 <ProfilePic />
                 <div className="flex md:flex-1 flex-col justify-center gap-4">
                     <div className="flex justify-between items-center gap-5 ">
